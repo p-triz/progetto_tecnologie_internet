b5e75c6b21704d0cc72ded42a59f9a2c5f3a310e
between16 <francescovescovi6@gmail.com>
2024-04-05 15:55:59 +0200

    ho aggiunto le pagine per le classifiche e relativo css

--- frontend/src/components/SecondPage.jsx
+++ frontend/src/components/SecondPage.jsx
@@ -1,15 +1,21 @@
 import Card from "./Card";
+import Scoreboard from "./Scoreboard";
 import './SecondPage.css'
 
 const App = () => {
-  const imageSrc = '../assets/react.svg';
-  const description = 'First Game';
+  const imageSrc = '../assets/react.svg'
+  const description = 'First Game'
   const description2 = 'Second Game'
+  const gameOneName = 'GAME1'
+  const gameTwoName = "GAME2"
+
 
   return (
     <div className="container">
+      <Scoreboard gameName={gameOneName} gameId="1"></Scoreboard>
       <Card imageSrc={imageSrc} description={description}  />
       <Card imageSrc={imageSrc} description={description2} />
+      <Scoreboard gameName={gameTwoName} gameId="2"></Scoreboard>
     </div>
   );
 };
