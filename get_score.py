87ebf2c0b01b5e618eb8c2059779197825bfb331
between16 <francescovescovi6@gmail.com>
2024-04-20 19:11:26 +0200

    fatto qualche cambiamento minore lato backend

--- get_score.py
+++ get_score.py
@@ -1,9 +1,9 @@
 from flask import jsonify, request
 
 def receive_game_score():
-    data = request.args.to_dict()
-    playername = data.get('playername')
-    score = data.get('score')
-    gameId = data.get('gameId')
+    data = request.get_json()
+    playername = data['playername']
+    score = data['score']
+    gameId = data['gameId']
 
     print(f"{playername} {score} {gameId}")
